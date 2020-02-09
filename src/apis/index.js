aca9a0f8f84ff02cb01d065fdb757a8900848e38
Ravik Ganguly <theravikganguly@gmail.com>
2020-02-10 00:42:05 +0530

    chore: deployment changes

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-const baseURL = "http://localhost:5000/api";
+const baseURL =
+  process.env.NODE_ENV === "production"
+    ? "https://examkhojo.herokuapp.com/api"
+    : "http://localhost:5000/api";
 
 export default axios.create({
   baseURL,
